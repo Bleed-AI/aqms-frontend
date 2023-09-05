097250107615dc69d560eb72749b6c04fb9f81a0
Abdullah Raheel <Abdullaharaheel@gmail.com>
2023-09-05 14:23:48 +0500

    updated link

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://quotamgmt.com"
+export const BASE_URL = "http://104.197.242.108:8000"
 //export const BASE_URL = "http://localhost:8000"
 export const APP_URL = "http://localhost:3000"
